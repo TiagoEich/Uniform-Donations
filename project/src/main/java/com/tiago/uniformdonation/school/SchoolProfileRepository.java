c5c1d456092efa53c3bd5c8e7d93ac31641e7f82
TiagoEich <tiagobackeseichh@gmail.com>
2026-02-26 20:59:56 -0300

    Creating SchoolProfileRepository

---
+++ project/src/main/java/com/tiago/uniformdonation/school/SchoolProfileRepository.java
@@ -0,0 +1,6 @@
+package com.tiago.uniformdonation.school;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SchoolProfileRepository extends JpaRepository <SchoolProfile, Long> {
+}
