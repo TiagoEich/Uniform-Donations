da9c3c99c609a9215e687aa7d0d5583083faa3c2
TiagoEich <tiagobackeseichh@gmail.com>
2026-02-26 20:59:19 -0300

    Refactoring User Entity

--- project/src/main/java/com/tiago/uniformdonation/user/User.java
+++ project/src/main/java/com/tiago/uniformdonation/user/User.java
@@ -23,6 +23,7 @@ public class User {
     @Column(nullable = false)
     private String passwordHash;
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private UserRole role;
 
